f27918d2b1dd8de182909a49990521fa3148777e
coiee <109278690+coiee@users.noreply.github.com>
2020-03-02 13:23:39 +0000

    faq

--- Package.swift
+++ Package.swift
@@ -5,3 +5,7 @@
 import PackageDescription
 
 let package = Package(
+    name: "CoinGecko",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
