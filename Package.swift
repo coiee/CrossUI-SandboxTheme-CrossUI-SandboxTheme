a983535909949dbe374a173278b6f71c173a660c
coiee <109278690+coiee@users.noreply.github.com>
2020-03-02 14:35:03 +0000

    Sync from mac-work

--- Package.swift
+++ Package.swift
@@ -9,3 +9,6 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
+            name: "CoinGecko",
+            targets: ["CoinGecko"]),
+    ],
