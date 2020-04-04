be1aca28b6bd3903154f5c61053c478d187722fc
coiee <109278690+coiee@users.noreply.github.com>
2020-04-04 18:53:53 +0000

    arduino

--- Sources/CoinGecko/Models/MarketData.swift
+++ Sources/CoinGecko/Models/MarketData.swift
@@ -12,3 +12,5 @@ public struct MarketContainer: Codable {
     
     public var marketCap: Double { return market_data.marketCap }
     public var totalVolume: Double { return market_data.totalVolume }
+    public var high24h: Double { return market_data.high24h }
+    public var low24h: Double { return market_data.low24h }
