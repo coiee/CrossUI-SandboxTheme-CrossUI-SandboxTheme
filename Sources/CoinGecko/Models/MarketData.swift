896bb860060f3defb3bdc124fe828f3c3efde25e
coiee <109278690+coiee@users.noreply.github.com>
2020-04-07 07:01:21 +0000

    Fixes #70

--- Sources/CoinGecko/Models/MarketData.swift
+++ Sources/CoinGecko/Models/MarketData.swift
@@ -14,3 +14,6 @@ public struct MarketContainer: Codable {
     public var totalVolume: Double { return market_data.totalVolume }
     public var high24h: Double { return market_data.high24h }
     public var low24h: Double { return market_data.low24h }
+    
+    private let market_data: MarketData
+}
