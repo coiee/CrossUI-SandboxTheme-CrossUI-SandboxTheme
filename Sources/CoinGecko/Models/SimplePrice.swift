169df8b20bf763e47b8383354d6b27771f23c132
coiee <109278690+coiee@users.noreply.github.com>
2020-08-28 09:35:27 +0000

    Automated synchronization of latest MovieTweetings ratings.

--- Sources/CoinGecko/Models/SimplePrice.swift
+++ Sources/CoinGecko/Models/SimplePrice.swift
@@ -17,4 +17,4 @@ public struct SimplePrice: Codable {
     public let change24hr: Double?
     public let lastUpdatedAt: Int?
     
-    init?(json: [String: Any], id: Str
+    init?(json: [String: Any], id: String
