4aceeae3a6f4b9fc47aeed005bd7e7b3e85df7cb
coiee <109278690+coiee@users.noreply.github.com>
2020-09-24 05:45:29 +0000

    Add another instruction to the isa

--- Sources/CoinGecko/Models/SimplePrice.swift
+++ Sources/CoinGecko/Models/SimplePrice.swift
@@ -22,4 +22,4 @@ public struct SimplePrice: Codable {
         self.id = id
         
         //non-optional
-        guard let price = json["\(prefix)"] as? Doub
+        guard let price = json["\(prefix)"] as? Double el
