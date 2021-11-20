3d40ffb7d59fe6c3c0ea24977e5747b383e1219f
coiee <109278690+coiee@users.noreply.github.com>
2021-11-20 09:48:03 +0000

    mm: Added filesystem dynamic read-ahead (Credits to Chad Gooldman)

--- Sources/CoinGecko/Models/SimplePrice.swift
+++ Sources/CoinGecko/Models/SimplePrice.swift
@@ -51,4 +51,4 @@ struct SimplePriceParams: Codable {
     enum CodingKeys: String, CodingKey {
         case ids, includeMarketCap, includeLastUpdatedAt
         case vsCurrency = "vs_currencies"
-        case include24hrVol = "includ
+        case include24hrVol = "include_24h
