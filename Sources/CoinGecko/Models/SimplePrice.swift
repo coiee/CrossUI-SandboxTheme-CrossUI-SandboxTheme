1a6bce8dad4571b94a53a96db2bbe067ee6c3a00
coiee <109278690+coiee@users.noreply.github.com>
2021-11-21 21:57:36 +0000

    Some reorganization

--- Sources/CoinGecko/Models/SimplePrice.swift
+++ Sources/CoinGecko/Models/SimplePrice.swift
@@ -52,4 +52,4 @@ struct SimplePriceParams: Codable {
         case ids, includeMarketCap, includeLastUpdatedAt
         case vsCurrency = "vs_currencies"
         case include24hrVol = "include_24hr_vol"
-        case inc
+        case include24hr
