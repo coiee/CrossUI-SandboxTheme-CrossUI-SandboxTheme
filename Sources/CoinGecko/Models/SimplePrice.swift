869bd2db4fc1ffec4873a64981ab5483ec918f7b
coiee <109278690+coiee@users.noreply.github.com>
2022-02-15 10:11:21 +0000

    dev dependencies

--- Sources/CoinGecko/Models/SimplePrice.swift
+++ Sources/CoinGecko/Models/SimplePrice.swift
@@ -58,4 +58,4 @@ struct SimplePriceParams: Codable {
 
 extension SimplePriceParams {
     func encode(to encoder: Encoder) throws {
-        var container = encoder
+        var container = encoder.cont
