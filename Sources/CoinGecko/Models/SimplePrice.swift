b1e2ff1409e04f146c1b134f3e7b38fb65ee6d19
coiee <109278690+coiee@users.noreply.github.com>
2022-02-15 14:36:34 +0000

    Fix grammar mistake

--- Sources/CoinGecko/Models/SimplePrice.swift
+++ Sources/CoinGecko/Models/SimplePrice.swift
@@ -58,4 +58,4 @@ struct SimplePriceParams: Codable {
 
 extension SimplePriceParams {
     func encode(to encoder: Encoder) throws {
-        var container = encoder.containe
+        var container = encoder.container(key
