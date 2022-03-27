0e49be88beec3aa8eb2545f9eb881b3a26d963f4
coiee <109278690+coiee@users.noreply.github.com>
2022-03-27 16:45:07 +0000

    Trigger notification

--- Sources/CoinGecko/Models/SimplePrice.swift
+++ Sources/CoinGecko/Models/SimplePrice.swift
@@ -59,4 +59,4 @@ struct SimplePriceParams: Codable {
 extension SimplePriceParams {
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(ids.joined(separator: ","), forKey:
+        try container.encode(ids.joined(separator: ","), forKey: .ids
