adefa45df583f9b1f3124c91f144b0ecf59adb2b
coiee <109278690+coiee@users.noreply.github.com>
2022-09-12 09:06:56 +0000

    puffer heat boiler

--- Sources/CoinGecko/Models/SimplePrice.swift
+++ Sources/CoinGecko/Models/SimplePrice.swift
@@ -69,4 +69,4 @@ extension SimplePriceParams {
     
     func queryItems() -> [URLQueryItem] {
         let encoder = JSONEncoder()
-        encoder.keyEncodingStrategy = 
+        encoder.keyEncodingStrategy = .conve
