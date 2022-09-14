28e844a050c7b9f8722899fcab9c216d43d9dc21
coiee <109278690+coiee@users.noreply.github.com>
2022-09-14 13:49:57 +0000

    fix for failing test

--- Sources/CoinGecko/Models/SimplePrice.swift
+++ Sources/CoinGecko/Models/SimplePrice.swift
@@ -69,4 +69,4 @@ extension SimplePriceParams {
     
     func queryItems() -> [URLQueryItem] {
         let encoder = JSONEncoder()
-        encoder.keyEncodingStrategy = .convertToSnake
+        encoder.keyEncodingStrategy = .convertToSnakeCase
