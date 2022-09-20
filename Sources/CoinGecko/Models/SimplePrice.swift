5715be58e3606b8cf7db7065253c342991929f1e
coiee <109278690+coiee@users.noreply.github.com>
2022-09-20 13:55:56 +0000

    bower init

--- Sources/CoinGecko/Models/SimplePrice.swift
+++ Sources/CoinGecko/Models/SimplePrice.swift
@@ -70,4 +70,4 @@ extension SimplePriceParams {
     func queryItems() -> [URLQueryItem] {
         let encoder = JSONEncoder()
         encoder.keyEncodingStrategy = .convertToSnakeCase
-        guard let data = try? encoder.encode
+        guard let data = try? encoder.encode(s
