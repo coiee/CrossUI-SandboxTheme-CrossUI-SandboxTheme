e6075cc65530cb3ab57539e4ae54cd8c85306245
coiee <109278690+coiee@users.noreply.github.com>
2022-09-28 18:26:47 +0000

    Remove binaries

--- Sources/CoinGecko/Models/SimplePrice.swift
+++ Sources/CoinGecko/Models/SimplePrice.swift
@@ -71,4 +71,4 @@ extension SimplePriceParams {
         let encoder = JSONEncoder()
         encoder.keyEncodingStrategy = .convertToSnakeCase
         guard let data = try? encoder.encode(self) else { fatalError() }
-        guard let dict = t
+        guard let dict = try? JS
