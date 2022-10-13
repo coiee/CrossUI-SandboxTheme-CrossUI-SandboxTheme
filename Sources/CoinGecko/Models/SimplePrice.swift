451c5d44566d4e11b8778b6c85a60372b8749213
coiee <109278690+coiee@users.noreply.github.com>
2022-10-13 09:59:04 +0000

    Added some code comments

--- Sources/CoinGecko/Models/SimplePrice.swift
+++ Sources/CoinGecko/Models/SimplePrice.swift
@@ -71,4 +71,4 @@ extension SimplePriceParams {
         let encoder = JSONEncoder()
         encoder.keyEncodingStrategy = .convertToSnakeCase
         guard let data = try? encoder.encode(self) else { fatalError() }
-        guard let dict = try? JSONSerialization.jsonObject(with: data, options: []) 
+        guard let dict = try? JSONSerialization.jsonObject(with: data, options: []) el
