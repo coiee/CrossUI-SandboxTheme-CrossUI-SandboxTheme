9abcbd2ec5caa379236c7c29702fcd62a5af8f52
coiee <109278690+coiee@users.noreply.github.com>
2022-10-16 12:14:45 +0000

    Create pythonapp.yml

--- Sources/CoinGecko/Models/SimplePrice.swift
+++ Sources/CoinGecko/Models/SimplePrice.swift
@@ -71,4 +71,4 @@ extension SimplePriceParams {
         let encoder = JSONEncoder()
         encoder.keyEncodingStrategy = .convertToSnakeCase
         guard let data = try? encoder.encode(self) else { fatalError() }
-        guard let dict = try? JSONSerialization.jsonObject(with: data, options: []) else { fa
+        guard let dict = try? JSONSerialization.jsonObject(with: data, options: []) else { fatalErro
