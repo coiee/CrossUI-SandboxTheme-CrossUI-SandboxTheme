0c52e0d80b64f1fa6ee901f0ee5f43c443495616
coiee <109278690+coiee@users.noreply.github.com>
2022-11-13 11:12:31 +0000

    Add .gitlab-ci.yml

--- Sources/CoinGecko/Resources.swift
+++ Sources/CoinGecko/Resources.swift
@@ -12,3 +12,9 @@ public enum Endpoint: String {
     case ping = "/ping"
     
     case supportedVs = "/simple/supported_vs_currencies"
+    case simplePrice = "/simple/price"
+    
+    case coinsList = "/coins/list"
+    case coinsMarketChart = "/coins/%@/market_chart"
+    case coin = "/coins/%@"
+}
