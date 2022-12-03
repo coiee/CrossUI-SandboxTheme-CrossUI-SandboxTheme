5d9da255f567626aaaa2ba9cb6d22cc883e22857
coiee <109278690+coiee@users.noreply.github.com>
2022-12-03 17:30:18 +0000

    Version bump to 0.2.13

--- Tests/CoinGeckoTests/BasicTests.swift
+++ Tests/CoinGeckoTests/BasicTests.swift
@@ -3,3 +3,10 @@ import XCTest
 @testable import CoinGecko
 
 final class BasicTests: XCTestCase {
+    
+    private let client = CoinGeckoClient()
+    
+    func testPing() {
+        let exp = XCTestExpectation()
+        let ping = Resources.ping { (result: Result<Pong, CoinGeckoError>) in
+            guard case .success(let pong) = result else { XCTFail(); exp.fulfill(); return }
