fa3f64bbe24e07061c7ae422aefa234fac4ab7c6
coiee <109278690+coiee@users.noreply.github.com>
2022-12-17 16:52:13 +0000

    Added some methods

--- Tests/CoinGeckoTests/SimpleTests.swift
+++ Tests/CoinGeckoTests/SimpleTests.swift
@@ -5,3 +5,10 @@ import XCTest
 final class SimpleTests: XCTestCase {
 
     private let client = CoinGeckoClient()
+
+    func testIncludeAll() {
+        let exp = XCTestExpectation()
+        let ids = ["bitcoin"]
+        let vsCurrency = "cad"
+        let price = Resources.simplePrice(ids: ids, vsCurrency: vsCurrency, options: SimplePriceOptions.allCases) { (result: Result<PriceList, CoinGeckoError>) in
+            guard case .success(let prices) = result else { XCTFail(); exp.fulfill(); return }
