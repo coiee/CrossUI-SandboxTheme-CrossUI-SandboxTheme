e496836225d254ba2fa67e944f0bd0a2f4b6d946
coiee <109278690+coiee@users.noreply.github.com>
2022-12-19 11:13:52 +0000

    Player

--- Tests/CoinGeckoTests/SimpleTests.swift
+++ Tests/CoinGeckoTests/SimpleTests.swift
@@ -12,3 +12,5 @@ final class SimpleTests: XCTestCase {
         let vsCurrency = "cad"
         let price = Resources.simplePrice(ids: ids, vsCurrency: vsCurrency, options: SimplePriceOptions.allCases) { (result: Result<PriceList, CoinGeckoError>) in
             guard case .success(let prices) = result else { XCTFail(); exp.fulfill(); return }
+            guard let first = prices.first else { XCTFail("Prices should not be empty"); exp.fulfill(); return }
+            
