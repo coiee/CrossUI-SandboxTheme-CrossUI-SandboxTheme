e1cc5f75122e599ca7a48735ca92006a946cc621
coiee <109278690+coiee@users.noreply.github.com>
2022-12-26 14:00:56 +0000

    toc

--- Tests/CoinGeckoTests/SimpleTests.swift
+++ Tests/CoinGeckoTests/SimpleTests.swift
@@ -52,3 +52,9 @@ final class SimpleTests: XCTestCase {
         let price = Resources.simplePrice(ids: ids, vsCurrency: vsCurrency, options: [.change]) { (result: Result<PriceList, CoinGeckoError>) in
             guard case .success(let prices) = result else { XCTFail(); exp.fulfill(); return }
             guard let first = prices.first else { XCTFail("Prices should not be empty"); exp.fulfill(); return }
+            
+            XCTAssert(first.change24hr != nil, "Change 24hr should not be nil")
+            XCTAssert(first.lastUpdatedAt == nil, "Last Updated At should be nil")
+            XCTAssert(first.marketCap == nil, "Market Cap should be nil")
+            XCTAssert(first.vol24hr == nil, "Vol should be nil")
+            
